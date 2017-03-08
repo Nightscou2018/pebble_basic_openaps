a5e00aefad12772361e667b3b05c2ce9246f0426
Luis Betancourt <betluis@gmail.com>
2017-03-08 12:46:31 -0600

    Update pebble_basic_openaps.py

--- pebble_basic_openaps.py
+++ pebble_basic_openaps.py
@@ -5,7 +5,6 @@ from libpebble2.communication.transports.serial import SerialTransport
 #from libpebble2.exceptions import TimeoutError
 #from libpebble2.services.notifications import Notifications
 
-
 pebble = libpebble2.PebbleConnection(SerialTransport("/dev/rfcomm0"))
 pebble.connect()
 pebble.run_async()
@@ -20,7 +19,8 @@ with open ("/root/myopenaps/monitor/glucose.json")as BG_Data:
 	Time_BG=datetime.datetime.strptime((d[0]["display_time"], "%Y-%m-%dT%H:%M:%S")
 	BG_Previous = d[1]["glucose"]
 	BG_Delta = BG - BG_Previous
-
+	Time_Gap_Minutes = (datetime.datetime.now()-Time_BG).total_seconds()/60
+					   
 with open ("/root/myopenaps/monitor/iob.json")as IOB_Data:
         d= json.load(IOB_Data)
         IOB=round(d[0]["iob"],1)
@@ -29,14 +29,10 @@ with open ("/root/myopenaps/monitor/temp_basal.json")as temp_basal_Data:
         d= json.load(temp_basal_Data)
         temp_basal=round(d["rate"],1)
 
-
-Time_Gap_Minutes = (datetime.datetime.now()-Time_BG).total_seconds()/60
-
 if Time_Gap_Minutes < 15:
 
 	msg_line1= str(BG) + "(" + str(BG_Delta) +  ")/" + str(IOB) + "/" + str(temp_basal)
 	msg_line2= str(Time_BG_py)[11:16]
-	
 else:
 	msg_line1 = "Old Data: " + str(Time_Gap_Minutes) + "m"
 	msg_line2 = str(BG) + "(" + str(BG_Delta) +  ")/" + str(IOB) + "/" + str(temp_basal)
